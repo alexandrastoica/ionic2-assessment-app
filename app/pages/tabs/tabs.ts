404a51b5a2136e4413665ab127e4758c1c7d2502
Alexandra Stoica <alexandrasto95@gmail.com>
2016-07-18 17:43:21 +0300

    adapt tabs navigation to the new navigation structure of the app

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -5,7 +5,7 @@ import {Tests} from '../tests/tests';
 import {HomePage} from '../home/home';
 import {CreateTestPage} from '../create-test/create-test';
 import {DisplayCreatedTestsPage} from '../display-created-tests/display-created-tests';
-import {DementiaSqlightService} from '../../services/dementiasqlight.service';
+import {Welcome} from '../welcome/welcome';
 
 
 @Component({
@@ -16,9 +16,8 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   //tab1Root: any = Sections;
-  tab1Root: any = CreateTestPage;
-  tab2Root: any = DisplayCreatedTestsPage;
-  tab3Root: any = Profile;
+  tab1Root: any = DisplayCreatedTestsPage;
+  tab2Root: any = Profile;
   //tab3Root: any = Tests;
   //tab4Root: any = HomePage;
 
