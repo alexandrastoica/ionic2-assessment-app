23e02aa3d67ba5768b198022682a08268ffea8f9
Alexandra Stoica <alexandrasto95@gmail.com>
2016-07-18 17:38:49 +0300

    improve navigation structure on display tests page and change create test page into modal

--- app/pages/display-created-tests/display-created-tests.ts
+++ app/pages/display-created-tests/display-created-tests.ts
@@ -5,12 +5,9 @@ import {Tests} from '../tests/tests';
 import {Sections} from "../sections/sections";
 import {SectionsQuestionsPage} from "../sections-questions/sections-questions";
 import {GetData} from "../../providers/get-data/get-data";
-/*
-  Generated class for the DisplayCreatedTestsPage page.
+import {CreateTestPage} from "../create-test/create-test";
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
 @Component({
   templateUrl: 'build/pages/display-created-tests/display-created-tests.html',
   providers: [GetData]
@@ -34,68 +31,75 @@ export class DisplayCreatedTestsPage {
         this.questionCount += data[i].questions.length;
       }
     });
+  }
 
+  getItems(ev) {
+    //TODO: write search function
   }
 
   onPageDidEnter() {
-    //console.log("Loaded content");
-       // this.platform.ready().then(() => {
-            this.createdTests = [];
-            this.dementiaSqlService.getCreatedTests(this.user_id)
-            .then(
-              data => {
-                this.createdTests = [];
-                if (data.res.rows.length > 0) {
-                  for (var i = 0; i < data.res.rows.length; i++) {
-                    let item = data.res.rows.item(i);
-                    this.dementiaSqlService.getAnsweredQuestions(item.id).then(
-                    data => {
-                      if (item) {
-                        let percentage = ((data.res.rows.length / this.questionCount) * 100).toFixed(1);
-                        //console.log("answered: " + data.res.rows.length);
-                        // console.log("qc: " + this.questionCount);
-                        //console.log("pertange: " + percentage);
-                        this.createdTests.push(new CreateTest(item.id, item.name, item.user_id, item.date, percentage));
-                      }
-                    });
-                  }
+      this.createdTests = [];
+      this.dementiaSqlService.getCreatedTests(this.user_id).then(data => {
+          if (data.res.rows.length > 0) {
+            for (var i = 0; i < data.res.rows.length; i++) {
+              let item = data.res.rows.item(i);
+              this.dementiaSqlService.getAnsweredQuestions(item.id).then(data => {
+                if (item) {
+                  let percentage = ((data.res.rows.length / this.questionCount) * 100).toFixed(1);
+                  this.createdTests.push(new CreateTest(item.id, item.name, item.user_id, item.date, percentage));
                 }
-            });
-      //  });
-    }
-
-    showDetail(id){
-      this.nav.push(Tests, {id: id});
-    }
-
-    showDetailSection(createdtest) {
-      let id = createdtest.id;
-      this.dementiaSqlService.getAnsweredQuestions(id).then(
-        data => {
-          let item = data.res.rows[data.res.rows.length-1];
-          if (!item) {
-            this.nav.push(Sections, {
-              testId: id
-            });
-          } else {
-          this.getData.getBySectionId(item.section)
-            .then(data => {
-             // console.log(data.questions);
-              if(data.questions.length >= (item.question_id + 1)) {
-                this.nav.push(SectionsQuestionsPage, {
-                  section: data,
-                  questions: data.questions,
-                  testId: item.test_id,
-                  next_question: item.question_id
-                });
-              } else {
-                this.nav.push(Sections, {
-                  testId: item.test_id
-                });
-              }
-            });
+              });
+            }
           }
-        });
-    }
+      });
+      
+  }
+
+  createAssesment(){
+      //create modal for assesment creation
+      let modal = Modal.create(CreateTestPage);
+      this.nav.present(modal);
+
+      //on dismiss if data not empty, push Section page to navigation and pass testid
+      modal.onDismiss(data => {
+        if(data){
+          this.nav.push(Sections, {testId: data});
+        }
+      });
+  }
+
+  showDetail(id){
+    this.nav.push(Tests, {id: id});
+  }
+
+  showDetailSection(createdtest) {
+    let id = createdtest.id;
+    this.dementiaSqlService.getAnsweredQuestions(id).then(
+      data => {
+        let item = data.res.rows[data.res.rows.length-1];
+        if (!item) {
+          this.nav.push(Sections, {
+            testId: id
+          });
+        } else {
+        this.getData.getBySectionId(item.section)
+          .then(data => {
+           // console.log(data.questions);
+            if(data.questions.length >= (item.question_id + 1)) {
+              this.nav.push(SectionsQuestionsPage, {
+                section: data,
+                questions: data.questions,
+                testId: item.test_id,
+                next_question: item.question_id
+              });
+            } else {
+              this.nav.push(Sections, {
+                testId: item.test_id
+              });
+            }
+          });
+        }
+      });
+  }
 
 }
